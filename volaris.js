5ca300627710396e86ab1ab398bb38370075647b
Luis Bajaña <luisbajana@Luiss-MacBook-Air.local>
2017-02-24 12:02:07 -0500

    Cambios en buscador

--- volaris.js
+++ volaris.js
@@ -9,4 +9,26 @@ $(document).ready(function(){
             $(this).toggleClass('open');
         }
     );
+
+    $(document).on('click','#buscador-volaris',function(e){
+      e.preventDefault();
+    });
+
+    $(document).on('mouseenter','.buscador-volaris',function(){
+      $(this).addClass('buscador-volaris-activo');
+      $(this).find('input').attr('placeholder','Pregunta a Volaris');
+      $(this).find('img').attr('src','img/ic_search_white_24px.png');
+    });
+    $(document).on('mouseleave','.buscador-volaris',function(){
+
+      if( $(this).find('input').val() === '' ){
+          $(this).removeClass('buscador-volaris-activo');
+          $(this).find('input').attr('placeholder','');
+          $(this).find('input').blur();
+          $(this).find('img').attr('src','img/ic_search_black_24px.png');
+      }
+
+    });
+
+
 });
